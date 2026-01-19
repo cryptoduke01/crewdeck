36df16bcf69bb35c5be753a4200861ad22f215fc
cryptoduke01 <egbolucheakachukwu@gmail.com>
2026-01-19 06:13:08 +0100

    Fixed deployment

--- app/admin/lock/page.tsx
+++ app/admin/lock/page.tsx
@@ -115,7 +115,9 @@ export default function AdminLockPage() {
                 {passcode.map((digit, index) => (
                   <input
                     key={index}
-                    ref={(el) => (inputRefs.current[index] = el)}
+                    ref={(el) => {
+                      inputRefs.current[index] = el;
+                    }}
                     type="text"
                     inputMode="numeric"
                     maxLength={1}
